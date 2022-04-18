0b91d4096860f2c116fc6f5f8bef3777a156e3ca
premganeshkrify <premganesh@krify.net>
2022-04-18 17:03:09 +0530

    sqlite done

---
+++ local_data_base/lib/db/notes_database.dart
@@ -0,0 +1,48 @@
+import 'package:local_data_base/model/note.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class NotesDatabase {
+  static final NotesDatabase instance = NotesDatabase._init();
+  static Database? _database;
+  NotesDatabase._init();
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB("noted.db");
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    // IF WE WANT TO CHANGE THE APPLICATION DIRECTORY WE CAN CHANGE BY
+    // CHANGING GETdAtABASEPATH
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const boolType = 'BOOLEAN NOT NULL';
+    const integerType = "INTEGER NOT NULL";
+    const textType = ' Text Not Null';
+    await db.execute('''CREATE TABLE $tableNotes(
+      ${NoteFields.id},$idType,
+      ${NoteFields.isImportant}),$boolType ,
+      ${NoteFields.number}
+      ${NoteFields.title}$textType,
+      ${NoteFields.description}$textType
+      ${NoteFields.time}$textType
+      ''');
+  }
+
+  Future<Note> create(Note note) async {
+    final db = await instance.database;
+    final id = await db.insert(tableNotes, note.toJson());
+    return note.copy(id: id);
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
