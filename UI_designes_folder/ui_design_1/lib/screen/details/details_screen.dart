e4d43a934a35e4afbbef216739e457bec553a272
PremGanesh <premganesh@krify.net>
2022-04-27 21:42:19 +0530

    UI DESIGN WORKING

---
+++ UI_designes_folder/ui_design_1/lib/screen/details/details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:ui_design_1/screen/details/components/body.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
