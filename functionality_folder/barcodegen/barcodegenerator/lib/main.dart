fe1a927ff2fbb6cd1e66f826fb3234b8e9134fdb
premganeshkrify <premganesh@krify.net>
2022-05-10 11:38:38 +0530

    bar code generation completed

---
+++ functionality_folder/barcodegen/barcodegenerator/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_barcodes/barcodes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'BarCode'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: SizedBox(
+          height: 200,
+          child: SfBarcodeGenerator(
+            value: 'welcome the secret world heahaahaha ',
+            symbology: QRCode(),
+            // showValue: true,
+          ),
+        )));
+  }
+}
